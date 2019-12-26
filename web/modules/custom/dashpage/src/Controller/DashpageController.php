d874f79ee17a34427febcde67caa76895c34370b
dongtianzen <dongtianzen@gmail.com>
2019-12-25 22:00:06 -0500

    Update DashpageController.php

--- web/modules/custom/dashpage/src/Controller/DashpageController.php
+++ web/modules/custom/dashpage/src/Controller/DashpageController.php
@@ -20,12 +20,16 @@ class DashpageController extends ControllerBase {
     $markup .= '<div class="row padding-0">';
       $markup .= '<div class="text-center">';
         $markup .= '<div class="margin-0">';
-          $markup .= '<span class="font-weight-400">';
-            $markup .=  'You are not authorized to access this page';
+          $markup .= '<span class="bs-callout bs-callout-danger font-weight-400">';
+            $markup .=  'Demo page';
           $markup .= '</span>';
         $markup .= '</div>';
       $markup .= '</div>';
     $markup .= '</div>';
+    $markup .= '<div class="bs-callout bs-callout-danger" id="callout-badges-ie8-empty">
+    <h4>Cross-browser compatibility</h4>
+    <p>Badges wont self collapse in Internet Explorer 8 because it lacks support for the <code>:empty</code> selector.</p>
+  </div>';
 
 
     $build = array(
