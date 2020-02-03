1b34bb29bd54a8c91446e877eed7c124c2919ef1
dongtianzen <dongtianzen@gmail.com>
2020-02-02 20:04:30 -0500

    Update DashpageController.php

--- web/modules/custom/dashpage/src/Controller/DashpageController.php
+++ web/modules/custom/dashpage/src/Controller/DashpageController.php
@@ -310,9 +310,9 @@ class DashpageController extends ControllerBase {
     $markup .= '<div class="row padding-0">';
       $markup .= '<div class="text-center">';
         $markup .= '<div class="margin-0">';
-          $markup .= $this->_brandPageTemplate();
-          $markup .= $this->_brandPageTemplate('CANON', 191, '日本佳能公司', '解码和解扰');
-          $markup .= $this->_brandPageTemplate();
+          $markup .= $this->_brandPageTemplate('bs-callout-primar', 'HARMONIC', 27, '美国哈雷公司', '广播级接收器和解码器');
+          $markup .= $this->_brandPageTemplate('bs-callout-danger', 'CANON', 191, '日本佳能公司', '解码和解扰');
+          $markup .= $this->_brandPageTemplate('bs-callout-warning', 'PHABRIX', 23, '英国丰播瑞', '研发IP测试，发生和监控设备');
         $markup .= '</div>';
       $markup .= '</div>';
     $markup .= '</div>';
@@ -323,10 +323,10 @@ class DashpageController extends ControllerBase {
   /**
    * @see Twitter Bootstrap 3.2.0 Callout CSS Styles
    */
-  public function _brandPageTemplate($brand_name = '', $brand_tid = '', $brand_text = '', $solution_name = '', $solution_tid = '') {
+  public function _brandPageTemplate($class = 'bs-callout-danger', $brand_name = '', $brand_tid = '', $brand_text = '', $solution_name = '', $solution_tid = '') {
     $output = '';
 
-    $output .= '<div class="bs-callout bs-callout-danger" id="callout-badges-ie8-empty">';
+    $output .= '<div class="bs-callout '. $class . '" id="callout-badges-ie8-empty">';
       $output .= '<div class="row padding-0">';
         $output .= '<div class="col-md-3">';
           $output .= '<a href= ' . base_path() . 'taxonomy/term/' . $brand_tid . '>';
@@ -344,81 +344,6 @@ class DashpageController extends ControllerBase {
           $output .= '<p>';
             $output .= $solution_name;
           $output .= '</p>';
-        $output .= '</div>';
-      $output .= '</div>';
-    $output .= '</div>';
-
-    return $output;
-  }
-
-  /**
-   * @see Twitter Bootstrap 3.2.0 Callout CSS Styles
-   */
-  public function _brandCanon() {
-    $output = '';
-
-    $output .= '<div class="bs-callout bs-callout-danger" id="callout-badges-ie8-empty">';
-      $output .= '<div class="row padding-0">';
-        $output .= '<div class="col-md-3">';
-          $output .= '<a href= ' . base_path() . 'taxonomy/term/191>';
-            $output .= '<h4>CANON</h4>';
-          $output .= '</a>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-4">';
-          $output .= '<p>日本XXX公司</p>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-5">';
-          $output .= '<p>解码和解扰</p>';
-        $output .= '</div>';
-      $output .= '</div>';
-    $output .= '</div>';
-
-    return $output;
-  }
-
-  /**
-   * @see Twitter Bootstrap 3.2.0 Callout CSS Styles
-   */
-  public function _brandHarmonic() {
-    $output = '';
-
-    $output .= '<div class="bs-callout bs-callout-primary" id="callout-badges-ie8-empty">';
-      $output .= '<div class="row padding-0">';
-        $output .= '<div class="col-md-3">';
-          $output .= '<a href= ' . base_path() . 'taxonomy/term/27>';
-            $output .= '<h4>HARMONIC</h4>';
-          $output .= '</a>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-4">';
-          $output .= '<p>美国哈雷公司</p>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-5">';
-          $output .= '<p>广播级接收器和解码器</p>';
-        $output .= '</div>';
-      $output .= '</div>';
-    $output .= '</div>';
-
-    return $output;
-  }
-
-  /**
-   * @see Twitter Bootstrap 3.2.0 Callout CSS Styles
-   */
-  public function _brandPhabrix() {
-    $output = '';
-
-    $output .= '<div class="bs-callout bs-callout-warning" id="callout-badges-ie8-empty">';
-      $output .= '<div class="row padding-0">';
-        $output .= '<div class="col-md-3">';
-          $output .= '<a href= ' . base_path() . 'taxonomy/term/23>';
-            $output .= '<h4>PHABRIX</h4>';
-          $output .= '</a>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-4">';
-          $output .= '<p>英国丰播瑞</p>';
-        $output .= '</div>';
-        $output .= '<div class="col-md-5">';
-          $output .= '<p>研发IP测试，发生和监控设备</p>';
         $output .= '</div>';
       $output .= '</div>';
     $output .= '</div>';
