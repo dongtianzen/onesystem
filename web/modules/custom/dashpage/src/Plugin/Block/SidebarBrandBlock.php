2d833d8ad469b5d17fee0efca7d3fa12af682122
dongtianzen <dongtianzen@gmail.com>
2020-05-06 10:01:11 -0400

    Update SidebarBrandBlock.php

--- web/modules/custom/dashpage/src/Plugin/Block/SidebarBrandBlock.php
+++ web/modules/custom/dashpage/src/Plugin/Block/SidebarBrandBlock.php
@@ -20,9 +20,30 @@ class SidebarBrandBlock extends BlockBase {
   public function build() {
     $build = [];
     $build['#theme'] = 'sidebar_brand_block';
-    $build['sidebar_brand_block']['#markup'] = 'Implement SidebarBrandBlock.';
+    $build['sidebar_brand_block']['#markup'] = $this->_SidebarBrandMenu();
 
     return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function _SidebarBrandMenu() {
+    $output = '';
+    $output .= '<nav role="navigation" aria-labelledby="block-showcase-lite-account-menu-menu" id="block-showcase-lite-account-menu" class="clearfix block block-menu navigation menu--account">';
+      $output .= '<h2 id="block-showcase-lite-account-menu-menu">';
+      $output .= 'User account menu';
+      $output .= '</h2>';
+      $output .= '<ul class="clearfix menu">';
+        $output .= '<li class="menu-item">';
+          $output .= '<a href="/showcase-lite/site/user/login" data-drupal-link-system-path="user/login" class="is-active">';
+            $output .= 'Log in';
+          $output .= '</a>';
+        $output .= '</li>';
+      $output .= '</ul>';
+    $output .= '</nav>';
+
+    return $output;
   }
 
 }
