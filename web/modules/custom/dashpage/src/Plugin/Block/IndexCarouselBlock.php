a861ec8af01a75c2bf4e247047f247d4be976d56
dongtianzen <dongtianzen@gmail.com>
2020-05-31 19:37:45 -0400

    Update IndexCarouselBlock.php

--- web/modules/custom/dashpage/src/Plugin/Block/IndexCarouselBlock.php
+++ web/modules/custom/dashpage/src/Plugin/Block/IndexCarouselBlock.php
@@ -41,61 +41,7 @@ class IndexCarouselBlock extends BlockBase {
    */
   public function _SidebarBrandMenu() {
     $output = '';
-
-    $path_args = \Drupal::service('flexinfo.setting.service')
-      ->getCurrentPathArgs();
-
-    if ($path_args[1] == 'newspage' && $path_args[3] == 'brand') {
-      $tid = $path_args[4];
-      $term = \Drupal::entityTypeManager()
-        ->getStorage('taxonomy_term')
-        ->load($tid);
-
-      if ($term) {
-        $output .= '<nav role="navigation" aria-labelledby="block-showcase-lite-account-menu-menu" id="block-showcase-lite-account-menu" class="clearfix block block-menu navigation menu--account">';
-          $output .= '<h2>';
-            $output .= $term->getName();
-          $output .= '</h2>';
-
-          $output .= $this->_SidebarBrandMenuLink($term);
-
-          $output .= '<ul class="clearfix menu">';
-            $output .= '<li class="menu-item">';
-            $output .= '<a>';
-              $output .= '其它事情';
-            $output .= '</a>';
-            $output .= '</li>';
-          $output .= '</ul>';
-        $output .= '</nav>';
-
-      }
-    }
-
-    return $output;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function _SidebarBrandMenuLink($term) {
-    $output = '';
-
-    if ($term) {
-      $entitys = \Drupal::service('flexinfo.field.service')
-        ->getFieldAllTargetIdsEntitys($term, 'field_brand_storymenu');
-
-      if ($entitys) {
-        foreach ($entitys as $key => $row) {
-          $link_path = '/newspage/term/brand/' . $term->id() . '/' . $row->id();
-
-          $output .= '<h2 class="height-38">';
-            $output .= '<span class="margin-left-12 float-left translateX-hover translateX-5">';
-              $output .= \Drupal::l($row->getName(), Url::fromUserInput($link_path));;
-            $output .= '</span>';
-          $output .= '</h2>';
-        }
-      }
-    }
+    $output = 'IndexCarouselBlock';
 
     return $output;
   }
