37ce37d86483d0fe303d114de6f3d44612417c03
dongtianzen <dongtianzen@gmail.com>
2020-05-31 21:22:15 -0400

    Carousel image

--- web/modules/custom/dashpage/src/Plugin/Block/IndexCarouselBlock.php
+++ web/modules/custom/dashpage/src/Plugin/Block/IndexCarouselBlock.php
@@ -31,55 +31,64 @@ class IndexCarouselBlock extends BlockBase {
     \Drupal::service('page_cache_kill_switch')->trigger();
 
     $build['#theme'] = 'sidebar_brand_block';
-    $build['sidebar_brand_block']['#markup'] = $this->_SidebarBrandMenu();
+    $build['sidebar_brand_block']['#markup'] = $this->_IndexCarouselHtml();
 
     return $build;
   }
 
   /**
    * {@inheritdoc}
+   * @see https://getbootstrap.com/docs/3.3/javascript/#carousel
    */
-  public function _SidebarBrandMenu() {
+  public function _IndexCarouselHtml() {
     $output = '';
-    $output = 'IndexCarouselBlock';
-    $output = '
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
+    $output .= '<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">';
+      // <!-- Indicators -->
+      $output .= '<ol class="carousel-indicators">';
+        $output .= '<li data-target="#carousel-example-generic" data-slide-to="0" class="active">';
+        $output .= '</li>';
+        $output .= '<li data-target="#carousel-example-generic" data-slide-to="1">';
+        $output .= '</li>';
+        $output .= '<li data-target="#carousel-example-generic" data-slide-to="2">';
+        $output .= '</li>';
+      $output .= '</ol>';
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          ...
-        </div>
+      // <!-- Wrapper for slides -->
+      $image_path_1 = drupal_get_path('module', 'dashpage') . '/image/47.png';
+      $image_path_1 = drupal_get_path('module', 'dashpage') . '/image/77.png';
+      $image_path_1 = drupal_get_path('module', 'dashpage') . '/image/79.png';
+      $output .= '<div class="carousel-inner" role="listbox">';
+        $output .= '<div class="item active">';
+          $output .= '<img src=" ' . . '" alt="...">';
+          $output .= '<div class="carousel-caption">';
+              $output .= '...';
+          $output .= '</div>';
+        $output .= '</div>';
+        $output .= '<div class="item">';
+          $output .= '<img src="" alt="...">';
+          $output .= '<div class="carousel-caption">';
+            $output .= '...';
+          $output .= '</div>';
+        $output .= '</div>';
+      $output .= '</div>';
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    ';
+      // <!-- Controls -->
+      $output .= '<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">';
+        $output .= '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true">';
+        $output .= '</span>';
+        $output .= '<span class="sr-only">';
+          $output .= 'Previous';
+        $output .= '</span>';
+      $output .= '</a>';
 
+      $output .= '<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">';
+        $output .= '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true">';
+        $output .= '</span>';
+        $output .= '<span class="sr-only">';
+          $output .= 'Next';
+        $output .= '</span>';
+      $output .= '</a>';
+    $output .= '</div>';
     return $output;
   }
 
