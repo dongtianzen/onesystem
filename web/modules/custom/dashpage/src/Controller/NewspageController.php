b7485d7423d648f25fae60d4191d22a10b092b01
dongtianzen <dongtianzen@gmail.com>
2020-06-07 18:54:39 -0400

    dev _getNewsPageHtml

--- web/modules/custom/dashpage/src/Controller/NewspageController.php
+++ web/modules/custom/dashpage/src/Controller/NewspageController.php
@@ -34,22 +34,9 @@ class NewspageController extends ControllerBase {
 
   /**
    * @return string
-   *   Return Hello string.
    */
-  public function _getTermBrandHtml($term_tid = NULL, $second_tid = NULL) {
+  public function _getNewsPageHtml($nodes = array()) {
     $output = NULL;
-
-    $query = \Drupal::service('flexinfo.querynode.service')
-      ->queryNidsByBundle('article');
-    $group = \Drupal::service('flexinfo.querynode.service')
-      ->groupStandardByFieldValue($query, $field_name = 'field_article_brand', $term_tid);
-    $query->condition($group);
-    $query->sort('created', 'DESC');
-    $query->pager(2);
-
-    $nids = \Drupal::service('flexinfo.querynode.service')
-      ->runQueryWithGroup($query);
-    $nodes = \Drupal::entityManager()->getStorage('node')->loadMultiple($nids);
 
     if ($nodes && is_array($nodes)) {
       foreach ($nodes as $node) {
@@ -71,6 +58,29 @@ class NewspageController extends ControllerBase {
         $output .= '</div>';
       }
     }
+
+    return $output;
+  }
+
+  /**
+   *
+   */
+  public function _getTermBrandHtml($term_tid = NULL, $second_tid = NULL) {
+    $output = NULL;
+
+    $query = \Drupal::service('flexinfo.querynode.service')
+      ->queryNidsByBundle('article');
+    $group = \Drupal::service('flexinfo.querynode.service')
+      ->groupStandardByFieldValue($query, $field_name = 'field_article_brand', $term_tid);
+    $query->condition($group);
+    $query->sort('created', 'DESC');
+    $query->pager(2);
+
+    $nids = \Drupal::service('flexinfo.querynode.service')
+      ->runQueryWithGroup($query);
+    $nodes = \Drupal::entityManager()->getStorage('node')->loadMultiple($nids);
+
+    $output = $this->_getNewsPageHtml($nodes);
 
     return $output;
   }
