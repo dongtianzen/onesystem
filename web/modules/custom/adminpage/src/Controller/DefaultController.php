203375a7e812dc8203bcd944649cdd87b4cfd203
dongtianzen <dongtianzen@gmail.com>
2020-07-04 17:45:35 -0400

    Update DefaultController.php

--- web/modules/custom/adminpage/src/Controller/DefaultController.php
+++ web/modules/custom/adminpage/src/Controller/DefaultController.php
@@ -36,6 +36,7 @@ class DefaultController extends ControllerBase {
   public function _guidePage() {
     $output = '';
     $output .= '<div class="row">';
+
       $output .= '<div class="bs-callout bs-callout-primary" id="callout-badges-ie8-empty">';
         $output .= '<div class="row padding-0">';
           $output .= '<div class="col-md-4">';
@@ -59,6 +60,55 @@ class DefaultController extends ControllerBase {
           $output .= '</div>';
         $output .= '</div>';
       $output .= '</div>';
+
+      $output .= '<div class="bs-callout bs-callout-primary" id="callout-badges-ie8-empty">';
+        $output .= '<div class="row padding-0">';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5 class="animated-hover">';
+              $output .= 'User';
+            $output .= '</h5>';
+          $output .= '</div>';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5>';
+              $output .= '<i class="fa fa-edit"></i>';
+              $internal_url = \Drupal\Core\Url::fromUserInput('/admin/people');
+              $output .= \Drupal::l('管理用户', $internal_url);
+            $output .= '</h5>';
+          $output .= '</div>';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5>';
+              $output .= '<i class="fa fa-clipboard"></i>';
+              $internal_url = \Drupal\Core\Url::fromUserInput('/admin/people/create');
+              $output .= \Drupal::l('添加用户', $internal_url);
+            $output .= '</h5>';
+          $output .= '</div>';
+        $output .= '</div>';
+      $output .= '</div>';
+
+      $output .= '<div class="bs-callout bs-callout-primary" id="callout-badges-ie8-empty">';
+        $output .= '<div class="row padding-0">';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5 class="animated-hover">';
+              $output .= 'Term';
+            $output .= '</h5>';
+          $output .= '</div>';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5>';
+              $output .= '<i class="fa fa-edit"></i>';
+              $internal_url = \Drupal\Core\Url::fromUserInput('/adminpage/views/content/node');
+              $output .= \Drupal::l('管理分类', $internal_url);
+            $output .= '</h5>';
+          $output .= '</div>';
+          $output .= '<div class="col-md-4">';
+            $output .= '<h5>';
+              $output .= '<i class="fa fa-clipboard"></i>';
+              $internal_url = \Drupal\Core\Url::fromUserInput('/node/add/article');
+              $output .= \Drupal::l('添加分类', $internal_url);
+            $output .= '</h5>';
+          $output .= '</div>';
+        $output .= '</div>';
+      $output .= '</div>';
+
     $output .= '</div>';
 
     return $output;
