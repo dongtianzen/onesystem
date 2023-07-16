1acfb1998cd72aa33ecfa2706271952249440043
Dong Tian <dongtianzen@users.noreply.github.com>
2023-07-16 12:39:20 -0400

    Update SideLinkBlock.php

--- web/modules/custom/siteinfo/src/Plugin/Block/SideLinkBlock.php
+++ web/modules/custom/siteinfo/src/Plugin/Block/SideLinkBlock.php
@@ -33,18 +33,33 @@ class SideLinkBlock extends BlockBase {
   public function switchLinkContent() {
     $output = NULL;
 
-    $term_names = $this->linkTermBrand();
-    foreach ($term_names as $key => $term_name) {
-      $terms = \Drupal::entityTypeManager()
-            ->getStorage('taxonomy_term')
-            ->loadByProperties(['name' => $term_name]);
-      if ($terms) {
-        $term = reset($terms);
-        if ($term) {
-          $output .= '<div class="side-link-block-wrapper">';
-            $output .= \Drupal::l($term_name, Url::fromUserInput('/taxonomy/term/' . $term->id()));
-          $output .= '</div>';
-        }
+    $current_path = \Drupal::service('path.current')->getPath();
+
+    $terms = [];
+    if ($current_path == '/dashpage/hello/presscentre') {
+      $terms = $this->getFullTermsFromVidName('news');
+    }
+    else if ($current_path == '/dashpage/hello/product') {
+      $terms = $this->getFullTermsFromVidName('brand');
+    }
+    else if ($current_path == '/dashpage/hello/solution') {
+      $terms = $this->getFullTermsFromVidName('solution');
+    }
+    else if ($current_path == '/dashpage/hello/service') {
+
+    }
+    else if ($current_path == '/node/429') {
+
+    }
+
+
+    // dump($current_path);
+    // dump($terms);
+    if ($terms && count($terms) > 0) {
+      foreach ($terms as $key => $term) {
+        $output .= '<div class="side-link-block-wrapper">';
+          $output .= \Drupal::l($term->getName(), Url::fromUserInput('/taxonomy/term/' . $term->id()));
+        $output .= '</div>';
       }
     }
 
@@ -99,7 +114,8 @@ class SideLinkBlock extends BlockBase {
    \Drupal::service('flexinfo.term.service')->getFullTermsFromVidName($vid);
    */
   public function getFullTermsFromVidName($vid = NULL) {
-    $tids = $this->getTidsFromVidName($vid);
+    $tids = \Drupal::service('flexinfo.term.service')
+      ->getTidsFromVidName($vid);
     $terms = \Drupal::entityTypeManager()
       ->getStorage('taxonomy_term')
       ->loadMultiple($tids);
