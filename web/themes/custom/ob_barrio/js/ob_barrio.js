0c34dd1ffe23703eb5e964a5edbab5311ee87aac
Dong Tian <dongtianzen@users.noreply.github.com>
2026-05-06 17:14:26 -0400

    one style

--- web/themes/custom/ob_barrio/js/ob_barrio.js
+++ web/themes/custom/ob_barrio/js/ob_barrio.js
@@ -35,4 +35,67 @@
     }
   };
 
+  Drupal.behaviors.obLangDropdown = {
+    attach: function (context, settings) {
+      once('ob-lang-dropdown', '.ob-lang-switcher nav.links', context)
+        .forEach(function (nav) {
+
+          var spans = nav.querySelectorAll('span.nav-link');
+          if (spans.length < 2) return;
+
+          var activeSpan = null;
+          var otherSpans = [];
+          spans.forEach(function (span) {
+            var a = span.querySelector('a.language-link');
+            if (a && a.classList.contains('is-active')) {
+              activeSpan = span;
+            } else {
+              otherSpans.push(span);
+            }
+          });
+
+          if (!activeSpan) {
+            activeSpan = spans[0];
+            otherSpans = Array.from(spans).slice(1);
+          }
+
+          function getLabel(span) {
+            var hl = span.getAttribute('hreflang');
+            if (hl === 'zh-hans') return '中文';
+            if (hl === 'en') return 'EN';
+            var a = span.querySelector('a');
+            return a ? a.textContent.trim() : hl;
+          }
+
+          function getFlag(span) {
+            var hl = span.getAttribute('hreflang');
+            if (hl === 'zh-hans') return '🇨🇳';
+            if (hl === 'en') return '🇬🇧';
+            return '🌐';
+          }
+
+          var dropdownHtml = '<div class="ob-lang-dd dropdown">'
+            + '<button class="ob-lang-dd__btn dropdown-toggle" type="button"'
+            + ' data-bs-toggle="dropdown" aria-expanded="false">'
+            + '<span class="ob-lang-dd__flag">' + getFlag(activeSpan) + '</span>'
+            + '<span class="ob-lang-dd__label">' + getLabel(activeSpan) + '</span>'
+            + '</button>'
+            + '<ul class="dropdown-menu dropdown-menu-end ob-lang-dd__menu">';
+
+          otherSpans.forEach(function (span) {
+            var a = span.querySelector('a');
+            if (!a) return;
+            dropdownHtml += '<li>'
+              + '<a class="dropdown-item ob-lang-dd__item" href="' + a.getAttribute('href') + '">'
+              + getFlag(span) + ' ' + getLabel(span)
+              + '</a></li>';
+          });
+
+          dropdownHtml += '</ul></div>';
+
+          nav.innerHTML = dropdownHtml;
+        });
+    }
+  };
+
 })(jQuery, Drupal, once);
